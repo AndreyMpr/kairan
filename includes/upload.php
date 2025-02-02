379f56f438be99f71f99c38256fb22c376729c0b
AndreyMpr <andreympr@gmail.com>
2025-02-02 22:38:03 +0900

    Create file path for storing uploads.

--- includes/upload.php
+++ includes/upload.php
@@ -4,6 +4,7 @@ $statusMsg = '';
   //file upload path
   $targetDir = "../uploads/";
   $fileName = basename($_FILES["file"]["name"]);
+  $targetFilePath = $targetDir . $fileName;
 
 echo $statusMsg;
 ?>
