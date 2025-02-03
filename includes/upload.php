6b863afb14ee254b19b7254dc87243352c303c51
AndreyMpr <andreympr@gmail.com>
2025-02-03 15:21:10 -0800

    Add if block that uploads file to the server.

--- includes/upload.php
+++ includes/upload.php
@@ -9,6 +9,10 @@ $statusMsg = '';
 
   if(isset($_POST["submit"]) && !empty($_FILES["file"]["name"])) {
     $allowTypes = array('pdf');
+    if(in_array($fileType, $allowTypes)){
+      if(move_uploaded_file($_FILES["file"]["tmp_name"], $targetFilePath)){
+        $statusMsg = "The file ".$fileName. " has been uploaded.";
+    }}
   }
   
 
