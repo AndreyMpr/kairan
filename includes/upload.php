2d9ee697fc73e011da740d273be824a9eaf75091
AndreyMpr <andreympr@gmail.com>
2025-02-03 15:22:02 -0800

    Add comments.

--- includes/upload.php
+++ includes/upload.php
@@ -8,8 +8,10 @@ $statusMsg = '';
   $fileType = pathinfo($targetFilePath, PATHINFO_EXTENSION);
 
   if(isset($_POST["submit"]) && !empty($_FILES["file"]["name"])) {
+    //allow certain file formats
     $allowTypes = array('pdf');
     if(in_array($fileType, $allowTypes)){
+      //upload file to server
       if(move_uploaded_file($_FILES["file"]["tmp_name"], $targetFilePath)){
         $statusMsg = "The file ".$fileName. " has been uploaded.";
     }}
