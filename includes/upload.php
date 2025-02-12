d1868debcc43c5705755abad19ca602978b8952d
AndreyMpr <andreympr@gmail.com>
2025-02-12 17:24:16 +0900

    Check if the file already exists, before uploading it.

--- includes/upload.php
+++ includes/upload.php
@@ -11,12 +11,16 @@ $statusMsg = '';
     //allow certain file formats
     $allowTypes = array('pdf');
     if(in_array($fileType, $allowTypes)){
-        //upload file to server
-      if(move_uploaded_file($_FILES["file"]["tmp_name"], $targetFilePath)){
-            $statusMsg = "PDFファイル".$fileName. "登録完了しました。";
-      }else{
-            $statusMsg = "アプロードする時にエラーが発生しました。";
-      }
+      // Check if file already exists
+      if (file_exists($target_file)) {
+        $statusMsg = "すみません、".$fileName. "フファイルは既に存在します。";
+      } 
+          //upload file to server
+        if(move_uploaded_file($_FILES["file"]["tmp_name"], $targetFilePath)){
+              $statusMsg = "PDFファイル".$fileName. "登録完了しました。";
+        }else{
+              $statusMsg = "アプロードする時にエラーが発生しました。";
+        }
     }else{
         $statusMsg = 'PDFファイル以外アプロードできません。';
     }
