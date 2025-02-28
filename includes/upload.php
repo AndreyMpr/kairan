7faf360338e717c0084757c76cc519187558a947
AndreyMpr <andreympr@gmail.com>
2025-02-28 23:49:42 +0900

    Update upload script.

--- includes/upload.php
+++ includes/upload.php
@@ -1,39 +1,40 @@
 <?php
 $statusMsg = '';
 
-  //file upload path
-  $targetDir = "../uploads/";
-  $fileName = basename($_FILES["file"]["name"]);
-  $targetFilePath = $targetDir . $fileName;
-  $fileType = pathinfo($targetFilePath, PATHINFO_EXTENSION);
+if(isset($_POST["submit"])) {
 
-  if(isset($_POST["submit"])) {
-    if(empty($_FILES["file"]["name"])) {
-      $statusMsg = "Please select file to upload";
-    }
-  }
+  if(empty($_FILES["file"]["name"])) {
+    $statusMsg = "Please select file to upload";
+  } else {
+    $targetDir = "../uploads/";
+    $fileName = basename($_FILES["file"]["name"]);
+    $targetFilePath = $targetDir . $fileName;
+    $fileType = pathinfo($targetFilePath, PATHINFO_EXTENSION);
 
-  if(isset($_POST["submit"]) && !empty($_FILES["file"]["name"])) {
     //allow certain file formats
     $allowTypes = array('pdf');
     $maxFileSize = 31457280;
+
     if(in_array($fileType, $allowTypes)){
       // Check if file already exists
-      if (file_exists($target_file)) {
+      if (file_exists($targetFilePath)) {
         $statusMsg = "すみません、".$fileName. "フファイルは既に存在します。ファイル名を変更、あるいは違うファイルを選択してください。";
-      } 
+      } else {
           //upload file to server
         if(move_uploaded_file($_FILES["file"]["tmp_name"], $targetFilePath)){
               $statusMsg = "PDFファイル".$fileName. "登録完了しました。";
         }else{
               $statusMsg = "アプロードする時にエラーが発生しました。";
         }
+      }
     }else{
         $statusMsg = 'PDFファイル以外アプロードできません。';
     }
-  }else{
-    $statusMsg = 'PDFファイルを選択せてください。';
   }
+} else {
+    $statusMsg = "";
+}
 
 //display status message
 echo $statusMsg;
+?>
