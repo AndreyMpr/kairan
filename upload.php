387423013f9e519b6210b1530435d4e055c35947
AndreyMpr <andreympr@gmail.com>
2025-03-09 18:53:14 +0900

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,2 +1,11 @@
 <?php 
 $statusMsg = ''; 
+
+// File upload directory 
+$targetDir = "uploads/"; 
+ 
+if(isset($_POST["submit"])){ 
+    if(!empty($_FILES["file"]["name"])){ 
+        $fileName = basename($_FILES["file"]["name"]); 
+        $targetFilePath = $targetDir . $fileName; 
+        $fileType = pathinfo($targetFilePath,PATHINFO_EXTENSION); 
