aed0b0e2794eca20c387b44b8ce119e05455f743
AndreyMpr <andreympr@gmail.com>
2025-03-12 21:40:39 +0900

    Fill in the test top page.

---
+++ test_space/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+        <title>PDF Upload</title>
+        </head>
+        <body>
+            <h1>Upload a PDF Document</h1>
+
+                <?php if(!empty($statusMsg)){ ?>
+                        <p class="statusMsg <?php echo !empty($msgClass)?$msgClass:''; ?>"><?php echo $statusMsg; ?></p>
+                            <?php } ?>
+
+                                <form action="upload.php" method="post" enctype="multipart/form-data">
+                                        <label for="pdfFile">Select PDF to upload:</label>
+                                                <input type="file" name="pdfFile" id="pdfFile" accept="application/pdf">
+                                                        <input type="submit" value="Upload" name="submit">
+                                                            </form>
+                                                            </body>
+                                                            </html>
+                                                            
