869b714e3ec119e7e0ac4e83616f7e71daf69937
AndreyMpr <andreympr@gmail.com>
2025-03-13 13:28:16 +0900

    Add db connection to the test uploads script.

--- test_space/test.php
+++ test_space/test.php
@@ -1,6 +1,10 @@
    <?php
+
    // db config
    $dbHost     = "localhost";
    $dbUsername = "root";
    $dbPassword = "";
    $dbName     = "pdf_database";
+
+   // connect to db
+   $db = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
