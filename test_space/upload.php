e8fbd659d7a7fe75114985ee8917be88911d2bbf
AndreyMpr <andreympr@gmail.com>
2025-03-18 13:32:29 +0900

    Add dictionary that links each sideource excel file with the column in a target file.

--- test_space/upload.php
+++ test_space/upload.php
@@ -20,3 +20,18 @@
    $targetDir = "test_uploads/";
    $fileName = basename($_FILES["pdfFile"] ["name"]);
    $targetFilePath = $targetDir . $fileName;
+
+   all_source_files = {
+      '515.xlsx': 3,   # Column D
+      '5151.xlsx': 4,  # Column E
+      '5152.xlsx': 5,  # Column F
+      '5153.xlsx': 6,  # Column G
+      '5154.xlsx': 7,  # Column H
+      '545.xlsx': 8,   # Column I
+      '5451.xlsx': 9,  # Column J
+      '5452.xlsx': 10, # Column K
+      '514.xlsx': 11,  # Column L
+      '544.xlsx': 12   # Column M
+   }
+
+   }
